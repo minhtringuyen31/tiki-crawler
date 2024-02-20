f38f4ef09ba4f621921e91cfad960c94d09fcbd8
unknown <minhtri.nguyenvo31@gmail.com>
2024-02-20 23:12:36 +0700

    update data

--- crawl_product_id.py
+++ crawl_product_id.py
@@ -75,4 +75,4 @@ for i in range(1, 11):
     time.sleep(random.randrange(3, 10))
 
 df = pd.DataFrame(product_id)
-df.to_csv('product_id.csv', index=False)
+df.to_csv('product_id_ncds.csv', index=False)
